dfd9cd5c6304ba968832f5a5e916c4814ba77fcc
Sadık Suat Özgül <ssozgul15@gmail.com>
2019-03-08 17:36:47 +0300

    GUI and DATABASE update
    - New Entry Boxes are added
    - Database modified. Now Storing: Day name, Date, Student Name, Start-stop Time, Course Type
    - Input and reading the database is working properly

--- private_teacher_schedule_quick.py
+++ private_teacher_schedule_quick.py
@@ -80,22 +80,24 @@ class Calendar:
         self.setup(self.year, self.month)
      
     def import_data(self):
-        c.execute('CREATE TABLE IF NOT EXISTS  TRIAL  (Date TEXT, Name TEXT)') #SQL syntax
+        c.execute('CREATE TABLE IF NOT EXISTS  TRIAL5  (Day VARCHAR, Date TEXT, Name VARCHAR, Start INT, Stop INT, Type VARCHAR)') #SQL syntax
         
 #        date = datetime.date(int(year.get()),int(month.get()), int(day.get())) #Date in format from 'import datetime'
 #
-        c.execute('INSERT INTO DENEME (Date, Name) VALUES (?, ?)',
-                  (self.day_name, self.price.get())) #Insert record into database.
+        c.execute('INSERT INTO TRIAL5 (Day, Date, Name, Start, Stop, Type) VALUES (?, ?, ?, ?, ?, ?)',
+                  (self.day_name, 
+                   calendar.month_name[self.month_selected] + str(self.day_selected) + str(self.year_selected),
+                   self.price.get(), self.timeStart.get(), self.timeStop.get(),self.courseType.get() )) #Insert record into database.
         con.commit()
         self.setup(self.year, self.month)
         
     def show_data(self):
-         c.execute('SELECT * FROM TRIAL') #now only selecting our beta database
+         c.execute('SELECT * FROM TRIAL5') #now only selecting our beta database
          
          self.frame =tk.Frame(self.parent)
-         self.frame.grid(row=2, column=12, rowspan = 4)
+         self.frame.grid(row=2, column=12, rowspan = 4, columnspan = 16)
          
-         self.Lb = tk.Listbox(self.frame, height = 8, width = 12,font=("arial", 12)) 
+         self.Lb = tk.Listbox(self.frame, height = 8, width = 40,font=("arial", 12)) 
          self.Lb.pack()
          
          self.entries = c.fetchall() # Gets the data from the table
@@ -152,7 +154,30 @@ class Calendar:
         self.name = tk.StringVar(self.parent)
 #        Entry for 'input' in GUI
         self.price = tk.Entry(self.parent, textvariable=self.name)
-        self.price.grid(row=8,column=12)        
+        self.price.grid(row=8,column=12)
+        
+        #adding entry fields for starting hour, end hour and lesson type
+        self.startTime = tk.StringVar(self.parent)
+#        Entry for 'input' in GUI
+        self.timeStart = tk.Entry(self.parent, textvariable=self.startTime, width= 6)
+        self.timeStart.grid(row=8,column=13) 
+        
+        self.stopTime = tk.StringVar(self.parent)
+#        Entry for 'input' in GUI
+        self.timeStop = tk.Entry(self.parent, textvariable=self.stopTime, width= 6)
+        self.timeStop.grid(row=8,column=14) 
+        
+        self.typeCourse = tk.StringVar(self.parent)
+#        Entry for 'input' in GUI
+        self.courseType = tk.Entry(self.parent, textvariable=self.typeCourse)
+        self.courseType.grid(row=8,column=15)
+        
+        #Labels for Entry Boxes
+        self.L1 = tk.Label(self.parent, text = "Student Name", font=("arial", 12)).grid(row=7, column=12)
+        self.L2 = tk.Label(self.parent, text = "Start", font=("arial",12)).grid(row=7, column=13)
+        self.L3 = tk.Label(self.parent, text = "Stop", font=("arial",12)).grid(row=7, column=14)
+        self.L4 = tk.Label(self.parent, text = "Type", font=("arial",12)).grid(row=7, column=15)
+        
          
         #to see the database
         imp = tk.Button(self.parent, text="Show_Data", command=self.show_data) #command will be added
